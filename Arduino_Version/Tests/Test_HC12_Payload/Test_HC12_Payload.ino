47c7392490ca47cfdfd1244a8dba05315d367ebe
Inverno <kieudong555@gmail.com>
2026-07-16 21:37:25 +0700

    Rewrite HC12 tests to pure UART passthrough to fix AT command parsing issues

--- Arduino_Version/Tests/Test_HC12_Payload/Test_HC12_Payload.ino
+++ Arduino_Version/Tests/Test_HC12_Payload/Test_HC12_Payload.ino
@@ -11,24 +11,18 @@ void setup() {
   // Initialize HC-12 on HardwareSerial 2
   HC12Serial.begin(9600, SERIAL_8N1, HC12_RX_PIN, HC12_TX_PIN);
   
-  Serial.println("--- HC-12 Payload Test ---");
-  Serial.println("Type something here and press Enter. It will be sent via Radio.");
-  Serial.println("Any data received from Radio will be printed here.");
+  Serial.println("--- HC-12 Passthrough Mode ---");
+  Serial.println("Cắm chân SET vào GND để vào chế độ AT.");
 }
 
 void loop() {
-  // Read from computer (USB) and send to HC-12 (Radio)
-  if (Serial.available()) {
-    String msg = Serial.readStringUntil('\n');
-    Serial.print("Sending: ");
-    Serial.println(msg);
-    HC12Serial.println(msg);
+  // Đọc từng byte từ máy tính và đẩy thẳng vào HC-12
+  while (Serial.available()) {
+    HC12Serial.write(Serial.read());
   }
 
-  // Read from HC-12 (Radio) and print to computer (USB)
-  if (HC12Serial.available()) {
-    String msg = HC12Serial.readStringUntil('\n');
-    Serial.print("Received from GCS: ");
-    Serial.println(msg);
+  // Đọc từng byte từ HC-12 và đẩy thẳng ra màn hình máy tính
+  while (HC12Serial.available()) {
+    Serial.write(HC12Serial.read());
   }
 }
